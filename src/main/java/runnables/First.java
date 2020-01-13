0fa0090aca32a10dc03304a4d73ce2084a32f57a
Simon Roberts <simon@dancingcloudservices.com>
2020-01-13 09:16:25 -0700

    BreakOne

---
+++ src/main/java/runnables/First.java
@@ -0,0 +1,22 @@
+package runnables;
+
+class MyJob implements Runnable {
+  int i = 0;
+  @Override
+  public void run() {
+    for (; i < 10_000; i++) {
+      System.out.println(Thread.currentThread().getName() +
+          " i is " + i);
+    }
+  }
+}
+public class First {
+  public static void main(String[] args) {
+    Runnable r = new MyJob();
+    Thread t1 = new Thread(r);
+    t1.start();
+    Thread t2 = new Thread(r);
+    t2.start();
+    System.out.println(Thread.currentThread().getName() + " exiting");
+  }
+}
---
+++ src/main/java/runnables/Second.java
@@ -0,0 +1,37 @@
+package runnables;
+
+class Stopper implements Runnable {
+  private volatile boolean stop = false;
+
+  @Override
+  public void run() {
+    System.out.println(Thread.currentThread().getName()
+        + " starting...");
+
+    while (!stop)
+      ;
+
+    System.out.println(Thread.currentThread().getName()
+        + " stopping...");
+  }
+
+  public boolean getStop() {
+    return stop;
+  }
+
+  public void stop() {
+    stop = true;
+  }
+}
+
+public class Second {
+  public static void main(String[] args) throws Throwable {
+    Stopper s = new Stopper();
+    new Thread(s).start();
+    System.out.println("Stopper started...");
+    Thread.sleep(1000);
+    System.out.println("Main woke up...");
+    s.stop();
+    System.out.println("Stop called on Stopper, stop value is " + s.getStop());
+  }
+}
